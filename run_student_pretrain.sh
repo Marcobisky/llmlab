bae335904c64fa5c27745a3a275cdeff0bd83a4e
Jinming Ren <3191293752@qq.com>
2026-06-02 18:43:33 +0800

    Change student pretrain params

--- run_student_pretrain.sh
+++ run_student_pretrain.sh
@@ -1,5 +1,5 @@
 python -m pip install -r requirements.txt
-python data.py --config config/expr_200k_depth4.yaml
+python data.py --config config/expr_500k_depth5.yaml
 python data.py --config config/eval_10k_depth5.yaml
 python pretrain.py --config config/student_pretrain.yaml
 
